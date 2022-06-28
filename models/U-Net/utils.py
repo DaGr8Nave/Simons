0a7f7ea96a5593b396f3770da7a8006abd79d0bd
Evan Wen <evan.h.wen@gmail.com>
2022-06-28 17:56:58 -0400

    Update utils.py

--- models/U-Net/utils.py
+++ models/U-Net/utils.py
@@ -62,12 +62,11 @@ def save_predictions_as_imgs(
             preds = torch.sigmoid(model(x))
             preds = (preds > 0.5).float().cpu()
             preds = torch.squeeze(preds)
-        real_image = np.zeros((480, 854, 3))
+        real_image = np.zeros((5, 480, 854, 3))
         for k in range(13):
-            print(preds.shape)
-            print(preds[k].shape)
-            print((preds[k] == 1).shape)
-            real_image[preds[k] == 1] = rgb_val[k]
+            print(preds[:,k,:,:].shape)
+            print((preds[:,k,:,:] == 1).shape)
+            real_image[:,preds[k] == 1] = rgb_val[k]
         real_image = torch.tensor(real_image)
         torchvision.utils.save_image(
             real_image, f"{folder}/pred_{idx}.png"
