043bbb07c6be54c2c9591771fcf5f2238a95e5bb
Evan Wen <evan.h.wen@gmail.com>
2022-06-29 14:08:36 -0400

    Update utils.py

--- models/U-Net/utils.py
+++ models/U-Net/utils.py
@@ -17,6 +17,7 @@ def load_checkpoint(checkpoint, model):
 def dice_coef(y_true, y_pred):
     y_true_f = y_true.flatten()
     y_pred_f = y_pred.flatten()
+    print((y_true_f*y_pred_f).shape)
     intersection = np.sum(y_true_f * y_pred_f)
     smooth = 0.0001
     return (2. * intersection + smooth) / (np.sum(y_true_f) + np.sum(y_pred_f) + smooth)
