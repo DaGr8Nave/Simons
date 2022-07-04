b2fb74ba7c47de6945509e466bec1f50baffc9bc
DaGr8Nave <36317925+DaGr8Nave@users.noreply.github.com>
2022-07-04 04:26:16 -0400

    Update eval.py

--- models/U-Net/eval.py
+++ models/U-Net/eval.py
@@ -49,7 +49,7 @@ val_transforms = A.Compose(
 val_dataset = VideoFrameDataset(val_paths, val_transforms)
 test_dataset = VideoFrameDataset(test_paths, val_transforms)
 model = UNet(n_channels=3, n_classes=13).to("cuda")
-load_checkpoint(torch.load("../../../../input/train-model/Simons/models/U-Net/my_checkpoint.pth.tar"), model)
+load_checkpoint(torch.load("../../../../input/unet-for-cholecseg8k/Simons/models/U-Net/my_checkpoint.pth.tar"), model)
 test_loader = DataLoader(test_dataset, batch_size=5)
 val_loader = DataLoader(val_dataset, batch_size=5)
 print("------------------ Test Set Results ------------------")
