270dfd957a82c23525a044e80e146a125021d6cd
DaGr8Nave <36317925+DaGr8Nave@users.noreply.github.com>
2022-07-25 22:04:51 -0400

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -16,7 +16,7 @@ mapping = {50:0,
           22: 10,
           33: 11,
           5: 12}
-CLASS_IDS = [0,1,2,3,4,5,6,7,8,9,10,11,12]
+CLASS_IDS = [0,5]
 class VideoFrameDataset(Dataset):
     def __init__(self, paths, transforms=None):
         self.image_dir = []
