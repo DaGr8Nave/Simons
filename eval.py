eed5ec61757a6874b46136b7cae399b2ca9e6447
DaGr8Nave <36317925+DaGr8Nave@users.noreply.github.com>
2022-08-09 00:21:01 -0400

    Update eval.py

--- eval.py
+++ eval.py
@@ -7,6 +7,7 @@ import torch.nn as nn
 import torch.optim as optim
 from models.UNet.unet_model import UNet
 from models.NestedUNet.nestedUNet import *
+from models.TransUNet import *
 from torch.utils.data import DataLoader
 from dataset import VideoFrameDataset
 import os 
