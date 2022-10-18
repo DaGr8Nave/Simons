940cbabb256cf4262863d06038cd300f7aa0f5db
Evan Wen <evan.h.wen@gmail.com>
2022-10-18 00:45:04 -0400

    Create segmenter_vit-s_mask_8x1_128x128_160k_cholecseg8k.py

---
+++ mmseg/configs/segmenter/segmenter_vit-s_mask_8x1_128x128_160k_cholecseg8k.py
@@ -0,0 +1,37 @@
+_base_ = [
+    '../_base_/models/segmenter_vit-b16_mask.py',
+    '../_base_/datasets/CholecSeg8k_config.py', '../_base_/default_runtime.py',
+    '../_base_/schedules/schedule_160k.py'
+]
+
+checkpoint = 'https://download.openmmlab.com/mmsegmentation/v0.5/pretrain/segmenter/vit_small_p16_384_20220308-410f6037.pth'  # noqa
+
+backbone_norm_cfg = dict(type='LN', eps=1e-6, requires_grad=True)
+model = dict(
+    pretrained=checkpoint,
+    backbone=dict(
+        img_size=(128, 128),
+        embed_dims=384,
+        num_heads=6,
+    ),
+    decode_head=dict(
+        type='SegmenterMaskTransformerHead',
+        in_channels=384,
+        channels=384,
+        num_classes=13,
+        num_layers=2,
+        num_heads=6,
+        embed_dims=384,
+        dropout_ratio=0.0,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0))
+    test_cfg=dict(crop_size=(128, 128), stride=(21, 21)))
+
+optimizer = dict(lr=0.001, weight_decay=0.0)
+
+data = dict(
+    # num_gpus: 8 -> batch_size: 8
+    samples_per_gpu=1,
+    train=dict(pipeline=train_pipeline),
+    val=dict(pipeline=test_pipeline),
+    test=dict(pipeline=test_pipeline))
