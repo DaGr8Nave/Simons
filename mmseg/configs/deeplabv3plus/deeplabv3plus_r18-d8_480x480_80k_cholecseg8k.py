91d32745f3e2d35f8b44c79e24e5dc826707d12e
Evan Wen <evan.h.wen@gmail.com>
2022-11-04 23:17:15 -0400

    Update deeplabv3plus_r18-d8_480x480_80k_cholecseg8k.py

--- mmseg/configs/deeplabv3plus/deeplabv3plus_r18-d8_480x480_80k_cholecseg8k.py
+++ mmseg/configs/deeplabv3plus/deeplabv3plus_r18-d8_480x480_80k_cholecseg8k.py
@@ -1,11 +1,11 @@
-_base_ = './deeplabv3plus_r50-d8_512x512_80k_potsdam.py'
+_base_ = [
+    '../_base_/models/deeplabv3plus_r50-d8.py',
+    '../_base_/datasets/CholecSeg8k_config.py', '../_base_/default_runtime.py',
+    '../_base_/schedules/schedule_80k.py'
+]
 model = dict(
-    pretrained='open-mmlab://resnet18_v1c',
     backbone=dict(depth=18),
-    decode_head=dict(
-        c1_in_channels=64,
-        c1_channels=12,
-        in_channels=512,
-        channels=128,
-    ),
-    auxiliary_head=dict(in_channels=256, channels=64))
+    decode_head=dict(c1_in_channels=64, c1_channels=12, in_channels=512, channels=128,num_classes=13),
+    auxiliary_head=dict(in_channels=256, channels=64, num_classes=13),
+    test_cfg=dict(mode='slide', crop_size=(480, 480), stride=(320, 320)))
+optimizer = dict(type='SGD', lr=0.004, momentum=0.9, weight_decay=0.0001)
